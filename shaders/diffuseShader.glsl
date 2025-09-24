e77de678fff3b78fbe74c7857cb725bf7282ce46
Timber Kent <timberkent@gmail.com>
2025-09-23 22:10:11 -0500

    Added rotating Triangle demo

--- shaders/diffuseShader.glsl
+++ shaders/diffuseShader.glsl
@@ -5,7 +5,6 @@ in vec3 FragPos;
 uniform vec3 objectColor;
 uniform vec3 lightColor;
 uniform vec3 lightPos;
-uniform vec3 lightPos2;
 
 void main()
 {
@@ -14,14 +13,10 @@ void main()
 
    vec3 norm = normalize(Normal);
    vec3 lightDir = normalize(lightPos - FragPos);
-   vec3 lightDir2 = normalize(lightPos2 - FragPos);
 
    float diff = max(dot(Normal, lightDir), 0.0);
    vec3 diffuse = diff * lightColor;
 
-   float diff2 = max(dot(Normal, lightDir2), 0.0);
-   vec3 diffuse2 = diff2 * lightColor;
-
-   vec3 calculation = (ambient + diffuse + diffuse2) * objectColor;
-    gl_FragColor = vec4(calculation, 1.0);
+   vec3 calculation = (ambient + diffuse) * objectColor;
+   gl_FragColor = vec4(calculation, 1.0);
 }  
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <memory>
 
 #include <SOGLR/SOGLR.hpp>
 
@@ -7,15 +8,53 @@ int main()
     Rendering::Renderer renderer;
     renderer.Begin();
     Rendering::Window window(800, 600, "SOGLR-ModelViewer");
+    if (!window.IsValid())
+    {
+        renderer.End();
+        exit(EXIT_FAILURE);
+    }
+    std::shared_ptr<Rendering::Shader> shader = std::make_shared<Rendering::Shader>(
+        "S:\\Users\\Timber\\Documents\\GitHub\\SOGLR-ModelViewer\\shaders\\vertexShader.glsl",
+        "S:\\Users\\Timber\\Documents\\GitHub\\SOGLR-ModelViewer\\shaders\\diffuseShader.glsl");
 
+    std::vector<float> triangle = {
+        -0.6f, -0.4f, 0.0f, 0.0f, 0.0f, 1.0f,
+        0.6f, -0.4f, 0.0f, 0.0f, 0.0f, 1.0f,
+        0.0f, 0.6f, 0.0f, 0.0f, 0.0f, 1.0f};
+
+    std::shared_ptr<Rendering::VertexBuffer> vertexBuff = std::make_shared<Rendering::VertexBuffer>(triangle);
+
+    std::shared_ptr<Rendering::IndexBuffer> indexBuffer = std::make_shared<Rendering::IndexBuffer>(std::vector<GLuint>{0, 1, 2});
+    auto obj1 = renderer.CreateRenderObject(indexBuffer, vertexBuff, shader);
+    float rotation = 1.0;
+
+    std::shared_ptr<Rendering::Camera> scene_camera = std::make_shared<Rendering::Camera>();
+    renderer.SetSceneCamera(scene_camera);
+    scene_camera->GetTransform().position_ = glm::vec3(0.0f, 0.0f, 3.0f);
+    renderer.BeginFrame();
     while (!window.ShouldClose())
     {
+        renderer.EndFrame();
         window.BeginFrame();
+        renderer.BeginFrame();
 
-        // Rendering code would go here
+        shader->Bind();
+
+        shader->SetUniform3f("objectColor", glm::vec3(1.0f, 1.0f, 1.0f));
+        shader->SetUniform3f("lightColor", glm::vec3(1.0f, 1.0f, 1.0f));
+        shader->SetUniform3f("lightPos", glm::vec3(1.0f, 1.0f, 1.0f));
+
+        rotation = rotation + (renderer.GetDeltaTime() * 360.0f);
+
+        obj1->SetRotation(glm::vec3(0.0f, rotation, 0.0f));
+        obj1->SetPosition(glm::vec3(0.0f, 0.0f, 0.0f));
+
+        renderer.DrawRenderList();
 
         window.EndFrame();
     }
+
+    renderer.EndFrame();
 
     renderer.End();
 
